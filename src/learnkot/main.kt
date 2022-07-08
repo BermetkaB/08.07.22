655bfb9f89219bfeb6b4da36d4ba4a7c6ebcec21
Bermet <bermet.bainaeva.ch@gmail.com>
2022-07-08 20:07:13 +0600

    data class id done

--- src/learnkot/main.kt
+++ src/learnkot/main.kt
@@ -2,9 +2,15 @@ package learnkot
 
 fun main() {
     val student=Student("Daniel",8,25487)
-    val student2=Student("Daniel",8,25487)
+    val student2=student.copy()
+   // val student2=Student("Daniel",8,25487)
+    val( name:String, age:Int, id:Int)=student
+   /* println(name)
+    println(age)
+    println(id)*/
 
-    println(student.toString())
+
+   println(student.toString())
     println(student.id)
     println(student.hashCode())
     println(student2.hashCode())
