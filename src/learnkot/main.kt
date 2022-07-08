0ce3b605c4eb95f00f26e60121baee665c712dbe
Bermet <bermet.bainaeva.ch@gmail.com>
2022-07-08 19:47:29 +0600

    some code

--- src/learnkot/main.kt
+++ src/learnkot/main.kt
@@ -2,5 +2,13 @@ package learnkot
 
 fun main() {
     val student=Student("Daniel",8,25487)
+    val student2=Student("Daniel",8,25487)
+
+    println(student.toString())
+    println(student.id)
     println(student.hashCode())
+    println(student2.hashCode())
+    println(student==student2)
+    println(student===student2)
+    student.printInfoOfStudent()
 }
--- src/learnkot/static.kt
+++ src/learnkot/static.kt
@@ -1,5 +1,8 @@
 package learnkot
 
-class Student(name:String, age:Int, id:Int){
+class Student( val name:String,  val age:Int,  val id:Int){
+fun printInfoOfStudent(){
+    println("Student's name is: $name  age $age and his id  $id")
 
+}
 }
